ece978c017bc1e8498a9fc6d474a49fa1971b65a
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-05-15 21:37:46 +0200

    Convert audio

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ use whisper_rs;
 fn main() {
     let jfk_audio = Path::new("./audio/jfk.wav");
     let mut reader = hound::WavReader::open(jfk_audio).expect("failed to open file");
-    println!("Wave spec: {:#?}", reader.spec());
+    #[allow(unused_variables)]
+    let hound::WavSpec {
+        channels,
+        sample_rate,
+        bits_per_sample,
+        ..
+    } = reader.spec();
     // Convert the audio to floating point samples.
     let mut audio = whisper_rs::convert_integer_to_float_audio(
         &reader
@@ -12,4 +18,13 @@ fn main() {
             .map(|s| s.expect("invalid sample"))
             .collect::<Vec<_>>(),
     );
+    if channels == 2 {
+        audio = whisper_rs::convert_stereo_to_mono_audio(&audio).unwrap();
+    } else if channels != 1 {
+        panic!(">2 channels unsupported");
+    }
+
+    if sample_rate != 16000 {
+        panic!("sample rate must be 16KHz");
+    }
 }
