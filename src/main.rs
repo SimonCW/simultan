0137875422513bfc4c20866c64afbbc4891fbde5
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-05-30 10:23:26 +0200

    Finish up example

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+use std::fs::File;
+use std::io::Write;
 use std::path::Path;
-use whisper_rs;
+use whisper_rs::{self, FullParams, SamplingStrategy, WhisperContext};
 
-fn main() {
+fn main() -> Result<(), &'static str> {
+    // ************ Setup Model
+    // Load a context and model.
+    let ctx = WhisperContext::new("models/ggml-base.en.bin").expect("failed to load model");
+    // Create a state
+    let mut state = ctx.create_state().expect("failed to create key");
+    // Create a params object for running the model.
+    // The number of past samples to consider defaults to 0.
+    let mut params = FullParams::new(SamplingStrategy::Greedy { best_of: 0 });
+
+    // Edit params as needed.
+    // Set the number of threads to use to 1.
+    params.set_n_threads(1);
+    // Enable translation.
+    params.set_translate(true);
+    // Set the language to translate to to English.
+    params.set_language(Some("en"));
+    // Disable anything that prints to stdout.
+    params.set_print_special(false);
+    params.set_print_progress(false);
+    params.set_print_realtime(false);
+    params.set_print_timestamps(false);
+
+    // ************ Setup Audio
     let jfk_audio = Path::new("./audio/jfk.wav");
     let mut reader = hound::WavReader::open(jfk_audio).expect("failed to open file");
     #[allow(unused_variables)]
@@ -23,8 +48,43 @@ fn main() {
     } else if channels != 1 {
         panic!(">2 channels unsupported");
     }
-
     if sample_rate != 16000 {
         panic!("sample rate must be 16KHz");
     }
+
+    // ******* Transcribe
+
+    // Run the model.
+    state.full(params, &audio[..]).expect("failed to run model");
+
+    // Create a file to write the transcript to.
+    let mut file = File::create("transcript.txt").expect("failed to create file");
+
+    // Iterate through the segments of the transcript.
+    let num_segments = state
+        .full_n_segments()
+        .expect("failed to get number of segments");
+    for i in 0..num_segments {
+        // Get the transcribed text and timestamps for the current segment.
+        let segment = state
+            .full_get_segment_text(i)
+            .expect("failed to get segment");
+        let start_timestamp = state
+            .full_get_segment_t0(i)
+            .expect("failed to get start timestamp");
+        let end_timestamp = state
+            .full_get_segment_t1(i)
+            .expect("failed to get end timestamp");
+
+        // Print the segment to stdout.
+        println!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
+
+        // Format the segment information as a string.
+        let line = format!("[{} - {}]: {}\n", start_timestamp, end_timestamp, segment);
+
+        // Write the segment information to the file.
+        file.write_all(line.as_bytes())
+            .expect("failed to write to file");
+    }
+    Ok(())
 }
